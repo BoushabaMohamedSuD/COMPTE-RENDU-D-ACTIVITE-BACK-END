5c2263b53fe1b7df7740a66f4f5ab8da7f0f847b
Boushaba Mohamed <med1998yz@gmail.com>
2020-06-12 02:24:02 +0200

    fetch data by code

--- Controller/Responsibilities/Elements/FetchData/FetchUserInfo.ts
+++ Controller/Responsibilities/Elements/FetchData/FetchUserInfo.ts
@@ -1,3 +1,5 @@
+import { FetchUserInfoBCode } from './../../../Factories/FetchUserInfo/FetchUserInfoBCode';
+import { FetchUserInfoEmail } from './../../../Factories/FetchUserInfo/FetchUserInfoEmail';
 import { ResponsibilitiesHolder } from './../../Holders/ResponsibilitiesHolder';
 
 export class FetchUserInfo {
@@ -5,10 +7,11 @@ export class FetchUserInfo {
     public static setFactorie(data: any, operation: any): ResponsibilitiesHolder {
 
         if (operation == 'email') {
-
+            return new FetchUserInfoEmail(data);
         } else if (operation == 'bcode') {
-
+            return new FetchUserInfoBCode(data);
         }
+
         throw new Error('no Factory selected');
 
     }
