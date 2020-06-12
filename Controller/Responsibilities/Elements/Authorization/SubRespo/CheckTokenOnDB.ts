e273e230266a3f08395ecb24481afb6b11618ec8
Boushaba Mohamed <med1998yz@gmail.com>
2020-06-12 17:10:02 +0200

    CheckTokenOnDB eub respo

---
+++ Controller/Responsibilities/Elements/Authorization/SubRespo/CheckTokenOnDB.ts
@@ -0,0 +1,65 @@
+import { SubRespoHolder } from './../../../Holders/SubRespoHolder';
+
+
+import jwt from 'jsonwebtoken';
+import { userInfo } from 'os';
+
+export class CheckTokenOnDB implements SubRespoHolder {
+
+    private Nextchaine!: SubRespoHolder;
+    private data: {
+        request: any,
+        elements: any,
+        response: any
+
+    };
+
+    constructor(
+        data: {
+            request: any,
+            elements: any,
+            response: any
+
+        }
+    ) {
+        this.data = data;
+    }
+
+    public setNextChaine(chaine: SubRespoHolder): SubRespoHolder {
+        this.Nextchaine = chaine;
+        return this.Nextchaine;
+    }
+
+    public process(): Promise<any> {
+        return new Promise((resolve, reject) => {
+
+            console.log("check token  on DB");
+
+            if (this.Nextchaine != null) {
+                console.log('going to next chaine');
+                this.Nextchaine.process()
+                    .then((resp) => {
+                        // resp is her false or true
+                        if (resp) {
+                            resolve(resp);
+                        } else {
+                            reject(resp);
+                        }
+
+                    })
+                    .catch((err) => {
+                        // console.log(err);
+                        //console.log('Error');
+                        reject(err);
+                    });
+            } else {
+                console.log('this is the end of the chaine');
+                resolve(true);
+            }
+
+
+
+        })
+    };
+
+}
