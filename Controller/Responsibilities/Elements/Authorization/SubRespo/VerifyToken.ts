f3898654172b65c7f3f3bc190631ab6462a36f09
Boushaba Mohamed <med1998yz@gmail.com>
2020-06-12 17:36:48 +0200

    verify token logic

--- Controller/Responsibilities/Elements/Authorization/SubRespo/VerifyToken.ts
+++ Controller/Responsibilities/Elements/Authorization/SubRespo/VerifyToken.ts
@@ -1,3 +1,4 @@
+import { JWT } from './../../../../../proprieties/JWT';
 import { SubRespoHolder } from './../../../Holders/SubRespoHolder';
 
 
@@ -34,32 +35,76 @@ export class VerifyToken implements SubRespoHolder {
         return new Promise((resolve, reject) => {
 
             console.log("Verify Token");
+            const reqToken = this.data.elements.reqtoken;
+            jwt.verify(reqToken, JWT.getInstance().getPassword(), (err: any, userData: any) => {
+                if (!err) {
 
-            if (this.Nextchaine != null) {
-                console.log('going to next chaine');
-                this.Nextchaine.process()
-                    .then((resp) => {
-                        // resp is her false or true
-                        if (resp) {
-                            resolve(resp);
+                    if (this.checkValidation(userData)) {
+
+                        if (this.Nextchaine != null) {
+                            console.log('going to next chaine');
+                            this.Nextchaine.process()
+                                .then((resp) => {
+                                    // resp is her false or true
+                                    if (resp) {
+                                        resolve(resp);
+                                    } else {
+                                        reject(resp);
+                                    }
+
+                                })
+                                .catch((err) => {
+                                    // console.log(err);
+                                    //console.log('Error');
+                                    reject(err);
+                                });
                         } else {
-                            reject(resp);
+                            console.log('this is the end of the chaine');
+                            resolve(true);
                         }
 
-                    })
-                    .catch((err) => {
-                        // console.log(err);
-                        //console.log('Error');
-                        reject(err);
-                    });
-            } else {
-                console.log('this is the end of the chaine');
-                resolve(true);
-            }
+                    } else {
+                        const errs = "ERorr on authorization";
+                        reject(errs);
+
+                    }
+
+
+                } else {
+                    console.log(err);
+                    reject(err);
+                }
+
+            })
+
 
 
 
         })
     };
+
+    private checkValidation(userdata: any): boolean {
+
+        const lastname = this.data.request.bodey.lastname;
+        const firstname = this.data.request.bodey.firstname;
+        const email = this.data.request.bodey.email;
+        const bcode = this.data.request.bodey.bcode;
+
+        if (userdata.lastname != lastname) {
+            return false;
+        }
+        if (userdata.firstname != firstname) {
+            return false;
+        }
+        if (userdata.email != email) {
+            return false;
+        }
+        if (userdata.bcode != bcode) {
+            return false;
+        }
+
+
+        return true;
+    }
 
 }
