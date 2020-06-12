609e60ca59a3c2290891f3565dda6e074e954b39
Boushaba Mohamed <med1998yz@gmail.com>
2020-06-12 16:15:09 +0200

    SUA false email

---
+++ Controller/Factories/SetUserActivity/SUA_FALSE_EMAIL.ts
@@ -0,0 +1,84 @@
+import { User } from '../../../Model/models/User';
+import { ResponsibilitiesHolder } from '../../Responsibilities/Holders/ResponsibilitiesHolder';
+
+
+
+export class SUA_FALSE_EMAIL implements ResponsibilitiesHolder {
+
+    private Nextchaine!: ResponsibilitiesHolder;
+    //private SubRespo!: SubRespoHolder;
+
+    private data: {
+        request: any,
+        elements: any,
+        response: any
+    };
+
+    constructor(
+        data: {
+            request: any,
+            elements: any,
+            response: any
+        }
+    ) {
+        this.data = data;
+    }
+
+
+    /* public setSubRespo(subRespo: SubRespoHolder): SubRespoHolder {
+       
+         return this.SubRespo
+     }*/
+
+    public setNextChaine(chaine: ResponsibilitiesHolder): ResponsibilitiesHolder {
+        this.Nextchaine = chaine;
+        return this.Nextchaine;
+    }
+
+    public process(): Promise<any> {
+        return new Promise((resolve, reject) => {
+
+            User.update({ IsActive: false }, { where: { Email: this.data.elements.email } })
+                .then((resp) => {
+                    this.data.elements = {
+                        ...this.data.elements,
+                        isactive: true
+                    }
+
+                    if (this.Nextchaine != null) {
+                        console.log('going to next chaine');
+                        this.Nextchaine.process()
+                            .then((resp: any) => {
+                                // resp is her false or true
+                                if (resp) {
+                                    resolve(resp);
+                                } else {
+                                    reject(resp);
+                                }
+
+                            })
+                            .catch((err: any) => {
+                                // console.log(err);
+                                //console.log('Error');
+                                reject(err);
+                            });
+                    } else {
+                        console.log('this is the end of the chaine');
+                        resolve(true);
+                    }
+
+                }).catch((err) => {
+                    console.log(err);
+                    reject(err);
+                })
+
+
+
+
+
+        })
+    };
+
+
+
+}
