badf542b62d3ec8d480d8c20bd90bfbb90abd4da
Boushaba Mohamed <med1998yz@gmail.com>
2020-06-12 18:23:42 +0200

    VUA ADMIN EMAIL

---
+++ Controller/Factories/UserAuthority/VerifyUserAuthority/VUA_ADMIN_EMAIL.ts
@@ -0,0 +1,100 @@
+import { User } from '../../../../Model/models/User';
+import { ResponsibilitiesHolder } from '../../../Responsibilities/Holders/ResponsibilitiesHolder';
+
+
+
+export class VUA_ADMIN_EMAIL implements ResponsibilitiesHolder {
+
+    private Nextchaine!: ResponsibilitiesHolder;
+    //private SubRespo!: SubRespoHolder;
+
+    private data: {
+        request: any,
+        elements: any,
+        response: any
+    };
+
+    constructor(
+        data: {
+            request: any,
+            elements: any,
+            response: any
+        }
+    ) {
+        this.data = data;
+    }
+
+
+    /* public setSubRespo(subRespo: SubRespoHolder): SubRespoHolder {
+       
+         return this.SubRespo
+     }*/
+
+    public setNextChaine(chaine: ResponsibilitiesHolder): ResponsibilitiesHolder {
+        this.Nextchaine = chaine;
+        return this.Nextchaine;
+    }
+
+    public process(): Promise<any> {
+        return new Promise((resolve, reject) => {
+
+            User.findOne({ where: { Email: this.data.elements.bcode } })
+                .then((user) => {
+
+
+                    if (user != null) {
+                        if (user.Authority == "admin") {
+                            if (this.Nextchaine != null) {
+                                console.log('going to next chaine');
+                                this.Nextchaine.process()
+                                    .then((resp: any) => {
+                                        // resp is her false or true
+                                        if (resp) {
+                                            resolve(resp);
+                                        } else {
+                                            reject(resp);
+                                        }
+
+                                    })
+                                    .catch((err: any) => {
+                                        // console.log(err);
+                                        //console.log('Error');
+                                        reject(err);
+                                    });
+                            } else {
+                                console.log('this is the end of the chaine');
+                                resolve(true);
+                            }
+
+                        } else {
+                            let err = "user is not an admin for make this operation";
+                            console.log(err);
+                            reject(err);
+
+
+                        }
+
+
+                    } else {
+                        let err = "user is null from vua admin email";
+                        console.log(err);
+                        reject(err);
+
+                    }
+
+
+                }).catch((err) => {
+                    console.log(err);
+                    reject(err);
+                })
+
+
+
+
+
+        })
+    };
+
+
+
+}
